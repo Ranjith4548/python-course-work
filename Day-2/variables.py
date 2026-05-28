28e0ba46fa70922d1c0f94a7f1f9c621e5d8f093
Ranjith4548 <paanchalaranjithkumar0@gmail.com>
2026-05-28 11:30:13 +0530

    Add files via upload

---
+++ Day-2/variables.py
@@ -0,0 +1,48 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> myvar
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    myvar
+NameError: name 'myvar' is not defined
+>>> myvar=10
+>>> myvar01=10
+>>> my_var=10
+>>> my var=10
+SyntaxError: invalid syntax
+>>> myvar@1=10
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+>>> if=10
+SyntaxError: invalid syntax
+>>> var=10
+>>> var=20
+>>> var
+20
+>>> 20
+20
+>>> a=b=c=10
+>>> a
+10
+>>> 10
+10
+>>> b
+10
+>>> 10
+10
+>>> c
+10
+>>> 10
+10
+>>> a,b,c=10,20,30
+>>> a
+10
+>>> 10
+10
+>>> b
+20
+>>> 20
+20
+>>> c
+30
+>>> 30
+30
