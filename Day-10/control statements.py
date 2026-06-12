9853b674f2a76504e6e3f406674fc9fcf26a9607
Paanchala Ranjith Kumar <paanchalaranjithkumar0@gmail.com>
2026-06-12 11:33:00 +0530

    Add files via upload

---
+++ Day-10/control statements.py
@@ -0,0 +1,118 @@
+seq=1,2,3,4
+for var in seq:
+    print(var)
+
+
+s='python programming'
+for ch in s:
+    print(ch)
+
+
+
+l=('sugar','salt','oil','eggs')
+for item in l:
+    print(item)
+
+
+
+
+
+t=('1.intro','2.Tokens','3.Data Types')
+for i in t:
+    print(i)
+
+
+
+
+s=['laptop','mouse','keyboard','iphone','charger']
+for i in s:
+    print(i)
+
+
+
+d={'name':'ajay','batch':'55','course':'pfs'}
+for i in d:
+    print(1,d[i])
+
+
+
+
+for i in range(1,11):
+    print(i)
+
+
+
+for i in range(20,51):
+    print(i)
+
+
+for i in range(2,51,2):
+    print(i)
+
+
+for i in range(5,101,5):
+    print(i)
+
+for i in range(20,0,-1):
+    print(i)
+
+for i in range(30,2,-3):
+    print(i)
+
+for i in range(6):
+    print(i)
+
+
+s='looping statements'
+for i in range(len(s)):
+    print(i,s[i])
+
+l=(1,2,3,4,5,6)
+for i in range(len(1)):
+    print(i,l[i])
+
+s='looping'
+for i in enumerate(s):
+    print(i[0],i[1])
+
+
+for i in range(10):
+    pass
+
+
+for i in range(10):
+    if i==5:
+        break
+    print(i)
+
+
+for i in range(10):
+    if i==5:
+        continue
+    print(i)
+
+
+s='looping statements'
+for i in s:
+    if i in 'aeiouAEIOU':
+        print(i)
+
+
+
+l=[56,76,32,3,34,2,3,5,97,45,13,23,45,23,98,76,32]
+for i in l:
+    if i%2==0:
+        print(i)
+
+d={'laptop':10,'charger':2,'keyboard':9,'phone':3,'tab':6,'mouse':4}
+for i in d:
+    if d[i]:
+        print(i)
+
+
+t=(1,2,3,4,5)
+for i in range(len(t)):
+    print(i*t[i])
+    
+
+    
